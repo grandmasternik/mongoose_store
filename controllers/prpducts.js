5aeafac1d43b9c388480bc3b9234d65d9eecba99
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-10 00:11:22 -0500

    controller in place plus connected

---
+++ controllers/prpducts.js
@@ -0,0 +1,86 @@
+// require and set up dependencies
+const express = require("express");
+const Product = require("../models/products");
+const productsRouter = express.Router(); //an object that provides "router functionality"
+
+productsRouter.get('/products/seed', (req, res) => {
+    productsRouter.get('/products', (req, res) => {
+    productsRouter.get('/products/new', (req, res) => {
+    productsRouter.delete('/products/:id', (req, res) => {
+    // productsRouter.delete('products/:id', 
+    productsRouter.put('/products/:id', (req, res) => {
+    productsRouter.post('/products', (req, res) => {
+    productsRouter.get('/products/:id/edit', (req, res) => {
+    productsRouter.get('/products/:id', (req, res) =>{
+
+// Seed
+productsRouter.get('/products/seed', (req, res) => {
+	Product.deleteMany({}, (error, allProducts) => {});
+	Product.create(productSeed, (error, data) => {
+		res.redirect('/products');
+	});
+});
+    
+//Index
+productsRouter.get('/products', (req, res) => {
+    Product.find({}, (error, allProducts) =>{
+        res.render('index.ejs', {
+            products: allProducts,
+        });
+    });
+});
+
+//New
+productsRouter.get('/products/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+//DELETE
+productsRouter.delete('/products/:id', (req, res) => {
+    Product.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect('/products');
+    });
+});
+        
+// Update
+productsRouter.put('/products/:id', (req, res) => {
+	if (req.body.readyToEat === 'on') {
+		req.body.readyToEat = true;
+	} else {
+		req.body.readyToEat = false;
+	}
+	res.send(req.body);
+});
+
+//Create 
+productsRouter.post('/products', (req, res) => {
+    if(req.body.completed === 'on') {
+        //if checked, req.body.completed is set to 'on'
+        req.body.completed = true;
+    } else {
+        //if not checked, req.body.completed is undefined
+        req.body.completed = false;
+}
+Product.create(req.body, (error, createdProduct) => {
+    res.redirect('/products');
+});
+});
+
+//EDIT
+productsRouter.get('/products/:id/edit', (req, res) => {
+	Product.findById(req.params.id, (error, foundProduct) => {
+		res.render('edit.ejs', {
+			product: foundProduct
+		});
+	});
+});
+
+// Show Page
+productsRouter.get('/products/:id', (req, res) =>{
+    Product.findById(req.params.id, (err, foundProduct) =>{
+        res.render('show.ejs', {product: foundProduct});
+    });
+});
+
+        // export functionality
+module.exports = productsRouter,
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 // ===== Dependencies ==== //
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const Product = require('./models/products.js');
+const Seed = require('./models/productSeed.js');
 // const PORT = 3000;
 
+//Middleware
+app.use(methodOverride('_method'));
+//Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended:true}));
 // ==== config mongoose ===== //
 //===Database Connection====//
 mongoose.connect(process.env.DATABASE_URL, {
@@ -15,46 +21,45 @@ mongoose.connect(process.env.DATABASE_URL, {
 	useCreateIndex: true,
 });
 
-
-// ==== Routes ==== //
-// Index
-app.get('/products', (req, res) => {
-	res.render('index.ejs');
-});
-
-// SHow
-app.get('/products/:id', (req, res) => {
-	Product.findById(req.params.id, (err, foundProduct) => {
-		res.render('show.ejs', {
-			product: foundProduct,
-		});
-	});
-});
-
-// New
-app.get('/products/new', (req, res) => {
-	res.render('new.ejs');
-});
-
+// // Index
+// app.get('/products', (req, res) => {
+	// 	res.render('index.ejs');
+	// });
+	
+	// // SHow
+	// app.get('/products/:id', (req, res) => {
+		// 	Product.findById(req.params.id, (err, foundProduct) => {
+			// 		res.render('show.ejs', {
+				// 			product: foundProduct,
+				// 		});
+				// 	});
+				// });
+				
+				// // New
+				// app.get('/products/new', (req, res) => {
+	
+// 	res.render('new.ejs');
+// });
 
 
-// Create
-app.post('/products', (req, res) => {
-	if (req.body.completed === 'on') {
-		//if checked, req.body.completed is set to 'on'
-		req.body.completed = true;
-	} else {
-		//if not checked, req.body.completed is undefined
-		req.body.completed = false;
-	}
-	Product.create(req.body, (error, createdProduct) => {
-		res.redirect('/products');
-	});
-});
 
-app.post('/products', (req, res) => {
-	res.send(req.body);
-});
+// // Create
+// app.post('/products', (req, res) => {
+// 	if (req.body.completed === 'on') {
+// 		//if checked, req.body.completed is set to 'on'
+// 		req.body.completed = true;
+// 	} else {
+// 		//if not checked, req.body.completed is undefined
+// 		req.body.completed = false;
+// 	}
+// 	Product.create(req.body, (error, createdProduct) => {
+// 		res.redirect('/products');
+// 	});
+// });
+
+// app.post('/products', (req, res) => {
+// 	res.send(req.body);
+// });
 
 // Update
 
@@ -65,6 +70,9 @@ const db = mongoose.connection
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+// ==== Controller ==== //
+const productsController = require('./controllers/products.js');
 
 //  ==== Web Server ===== //
 const PORT = process.env.PORT;
